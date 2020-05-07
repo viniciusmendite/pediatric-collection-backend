d7c0eb8b8fa33fca13e1f55e08aabc4727510c7a
Vinicius Mendite <vinicius.miff@gmail.com>
2020-05-07 10:04:55 -0300

    rotas PUT, DELETE e retorno de um único objeto SHOW

--- src/controllers/ConsultationController.js
+++ src/controllers/ConsultationController.js
@@ -9,9 +9,30 @@ module.exports = {
     return res.json(consultation);
   },
 
+  async show(req, res) {
+    const consultation = await Consultation.findById(req.params.id);
+
+    return res.json(consultation);
+  },
+
   async store(req, res) {
     const consultation = await Consultation.create(req.body);
 
     return res.json(consultation);
+  },
+  async update(req, res) {
+    const consultation = await Consultation.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    return res.json(consultation);
+  },
+
+  async delete(req, res) {
+    const consultation = await Consultation.findByIdAndRemove(req.params.id);
+
+    return res.send();
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ const routes = require("express").Router();
 const ConsultationController = require("./controllers/ConsultationController");
 
 routes.get("/consultation", ConsultationController.index);
+routes.get("/consultation/:id", ConsultationController.show);
 routes.post("/consultation", ConsultationController.store);
+routes.put("/consultation/:id", ConsultationController.update);
+routes.delete("/consultation/:id", ConsultationController.delete);
 
 module.exports = routes;
