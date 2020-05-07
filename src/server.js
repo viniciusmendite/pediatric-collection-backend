d3092964c4145eeb6644c695c07b951cec76fd9c
Vinicius Mendite <vinicius.miff@gmail.com>
2020-05-07 09:36:31 -0300

    adicionando o cors

--- src/server.js
+++ src/server.js
@@ -2,18 +2,23 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // Conexão com o MongoDB
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// chamando o model
 require("./models/Consultation");
 
+// chamando as rotas
 const routes = require("./routes");
 
 app.use(routes);
